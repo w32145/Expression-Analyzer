196381a6925923cc5e860a82097f25b8fce9140f
Will Schroeder <willschroeder@Wills-MacBook-Pro.local>
2026-07-24 20:53:33 -0500

    Fixed bug

--- methods.h
+++ methods.h
@@ -2,6 +2,7 @@
 #define METHODS_H
 char** tokenize(char* input, int* tokenCnt);
 char** postfix(char** tokens, int len, int* postLen);
+int solve(char **rpn, int len);
 int isNumber(char* token);
 int isOp(char c);
 #endif
